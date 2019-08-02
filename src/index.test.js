b2f8c1940e33bc78533521a1b03603e1565f3931
aphremthomas <aphremthomas@gmail.com>
2019-08-02 23:56:07 +0530

    sample test file added

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+
+describe('index component',()=>{
+    it('simply',()=>{
+        let out=true;
+        expect(out).toBe(true);
+    })
+})
