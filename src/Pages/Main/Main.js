1ea897b008a500c2e4d6c728f5d6f802c8834064
APHREM THOMAS <aphremthomas@macbooks-MacBook-Pro-3.local>
2019-09-07 12:13:56 +0530

    hooks added (sample)

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,10 +1,14 @@
 
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
 
 export default function Main() {
+    const [age, incAge]=useState(0);
     return (
         <div>
             {"main component"}
+            <h1>{`age is ${age}`}</h1>
+            <button onClick={()=>incAge(age+1)}>{'+'}</button>
         </div>
     )
 }
