4d3afc027207d5d8a47c8bf574937e78e46b6da1
APHREM THOMAS <aphremthomas@gmail.com>
2019-12-07 13:11:33 +0530

    using hooks to access store and dispatch actions

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,15 +1,19 @@
 
 import React from 'react';
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import {useSelector,useDispatch} from 'react-redux';
 
-export default function Main() {
+export default function Main(props) {
     const [age, incAge]=useState(0);
+    const count = useSelector(st=>st.counter);
+    const dispatch = useDispatch();
+    useEffect(()=>console.log("changed"),[count]);
     return (
         <div>
             {"main component"}
-            <h1>{`age is ${age}`}</h1>
-            <button onClick={()=>incAge(prevAge=>prevAge+1)}>{'+'}</button>
+            <h1>{`age is ${count}`}</h1>
+            <button onClick={()=>dispatch({type:'INC_COUNTER'})}>{'+'}</button>
             <Link to='/dummy'>Dummy</Link>
         </div>
     )
