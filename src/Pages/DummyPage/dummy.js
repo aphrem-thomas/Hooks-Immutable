2e8d6a06416768a3c37999e7adbbc7585dafd04f
APHREM THOMAS <aphremthomas@gmail.com>
2020-01-22 18:18:33 +0530

    using refs

--- src/Pages/DummyPage/dummy.js
+++ src/Pages/DummyPage/dummy.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class Dummy extends Component {
-    
+    constructor(props){
+       super(props);
+    }
+    componentDidMount(){
+        if(this.inpRef)
+            this.inpRef.focus();
+        console.log(this.inpRef);
+    }
     render() {
         return (
             <div>
                 dummy
+                <input type="text" ref={(o)=>{this.inpRef=o}}/>
             </div>
         )
     }
